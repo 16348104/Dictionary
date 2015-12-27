98b3fd40f5e448f627c260d4b5d83bc9e441c29e
16348104 <16348104@QQ.com>
2015-12-27 23:05:10 +0800

    spring mvc 3

--- src/main/java/com/dict/controller/BaseController.java
+++ src/main/java/com/dict/controller/BaseController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.dict.controller;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
 
--- src/main/java/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package controller;
-
-/**
- * Created by Administrator on 2015/12/27.
- */
-public class UserController {
-}
